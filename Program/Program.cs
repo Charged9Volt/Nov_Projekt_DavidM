ddf5f55ad4d107e4e351a94cda73f2510305ed68
Charged9Volt <61156947+Charged9Volt@users.noreply.github.com>
2020-11-17 12:56:32 +0100

    Förberedelse/Start

---
+++ Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Raylib_cs;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800,600,"Game engien");
+            Raylib.SetTargetFPS(60);
+            
+          
+        }
+    }
+}
